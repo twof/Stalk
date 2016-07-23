b93a0fd43f0efa6785e3daea66e66a3c42926586
twof <fabiobean2@gmail.com>
2016-07-23 16:12:57 -0700

    Added implementation

--- stalk/ViewController.swift
+++ stalk/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController, PPKControllerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         PPKController.enableWithConfiguration(APP_KEY, observer:self)
+        PPKController.enableProximityRanging()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -24,7 +25,35 @@ class ViewController: UIViewController, PPKControllerDelegate {
 
     func PPKControllerInitialized() {
         // ready to start discovering nearbys
+        let myDiscoveryInfo = "Hello from Swift!, Hey Justin!".dataUsingEncoding(NSUTF8StringEncoding)
+        PPKController.startP2PDiscoveryWithDiscoveryInfo(myDiscoveryInfo, stateRestoration: false)
     }
 
+    func p2pPeerDiscovered(peer: PPKPeer!) {
+        let discoveryInfoString = NSString(data: peer.discoveryInfo, encoding:NSUTF8StringEncoding)
+        NSLog("%@ is here with discovery info: %@", peer.peerID, discoveryInfoString!)
+    }
+    
+    func p2pPeerLost(peer: PPKPeer!) {
+        NSLog("%@ is no longer here", peer.peerID)
+    }
+    
+    func discoveryInfoUpdatedForPeer(peer: PPKPeer!) {
+        let discoveryInfo = NSString(data: peer.discoveryInfo, encoding: NSUTF8StringEncoding)
+        NSLog("%@ has updated discovery info: %@", peer.peerID, discoveryInfo!)
+        let myDiscoveryInfo = "Hello again!".dataUsingEncoding(NSUTF8StringEncoding)
+        PPKController.pushNewP2PDiscoveryInfo(myDiscoveryInfo)
+
+    }
+    
+    func proximityStrengthChangedForPeer(peer: PPKPeer!) {
+        if (peer.proximityStrength.rawValue > PPKProximityStrength.Weak.rawValue) {
+            NSLog("%@ is in range, do something with it", peer.peerID);
+        }
+        else {
+            NSLog("%@ is not yet in range", peer.peerID);
+        }
+    }
 }
+
 
