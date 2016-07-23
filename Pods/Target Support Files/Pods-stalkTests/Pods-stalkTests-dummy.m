fb3ab09b418596560022f255f8c5c05b7bfc2747
twof <fabiobean2@gmail.com>
2016-07-23 13:27:18 -0700

    Added podfiles

---
+++ Pods/Target Support Files/Pods-stalkTests/Pods-stalkTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_stalkTests : NSObject
+@end
+@implementation PodsDummy_Pods_stalkTests
+@end
---
+++ Pods/Target Support Files/Pods-stalkUITests/Pods-stalkUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_stalkUITests : NSObject
+@end
+@implementation PodsDummy_Pods_stalkUITests
+@end
