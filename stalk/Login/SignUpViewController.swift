0368bc7d227d7a08d7be3faa4e3b8f532f07a35d
Justin Lee <justinjlee99@gmail.com>
2016-07-24 10:30:48 -0700

    initial signup

---
+++ stalk/Login/SignUpViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  stalk
+//
+//  Created by Clara Hwang on 7/24/16.
+//  Copyright © 2016 carl. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+	
+	@IBAction func onSignUpButtonTap(sender: AnyObject) {
+	}
+	@IBAction func onLoginButtonTap(sender: AnyObject) {
+	}
+	
+	@IBOutlet weak var usernameLabel: UITextField!
+	
+	@IBOutlet weak var passwordTextField: UITextField!
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+	
+}
